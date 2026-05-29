36eb76f1e306550c997d4714bdcbad69096e32a2
ShahajiJadhav <64009305+ShahajiJadhav@users.noreply.github.com>
2026-05-29 09:40:17 +0530

    Update chartink_telegram_signal.py

--- chartink_telegram_signal.py
+++ chartink_telegram_signal.py
@@ -87,8 +87,8 @@ def log(msg: str):
     logger.info(msg)
 
 # ===================== PAYLOADS =====================
-buy_payload  = {"scan_clause": '''( {1339018} (  [0] 5 minute volume *  [0] 5 minute "high+low/2" >  700000000 and  [0] 5 minute close <  1500 and  [0] 5 minute close >  [0] 5 minute open and  abs(  [0] 5 minute close -  [0] 5 minute open ) >  [0] 5 minute low *  0.015 ) )'''}
-sell_payload = {"scan_clause": '''( {1339018} (  [0] 5 minute volume *  [0] 5 minute "high+low/2" >  700000000 and  [0] 5 minute close <  1500 and  [0] 5 minute close <  [0] 5 minute open and  abs(  [0] 5 minute close -  [0] 5 minute open ) >  [0] 5 minute low *  0.015 ) )'''}
+buy_payload  = {"scan_clause": '''( {1339018} (  [0] 5 minute count( 5, 1 where  [0] 5 minute close <  [-1] 5 minute close *  0.92 ) >=  1 and  [0] 5 minute close >  [0] 5 minute open and  [0] 5 minute volume *  [0] 5 minute "high+low/2" >  10000000 ) )'''}
+sell_payload = {"scan_clause": '''( {1339018} (  [0] 5 minute count( 5, 1 where  [0] 5 minute close >  [-1] 5 minute close *  1.08 ) >=  1 and  [0] 5 minute volume *  [0] 5 minute "high+low/2" >  10000000 and  [0] 5 minute close <  [0] 5 minute open ) )'''}
 
 # ===================== HELPERS =====================
 def parse_cookie(blob: str) -> dict:
